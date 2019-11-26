875c9eb28968b0a4d0dfab1d8c32a04fb5521466
Aditya Sundaram <adisundar02@gmail.com>
2019-11-25 22:48:54 -0500

    Add files via upload

---
+++ USACO/Contests/Gold/2018-2019/Contest 4/gold19open2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+
+int main()
+{
+    freopen("walk.in","r",stdin);
+    freopen("walk.out","w",stdout);
+    cin >> n >> k;
+    cout << 2019201997-48*n-84*(k-1) << endl;
+}
